7a275346b822c588cb0b4b0ea170a6a1b5b063de
d-beloved <morayodeji@gmail.com>
2018-10-29 17:39:48 +0100

    Set up testing with jest and enzyme
    [Delivers #161550361]

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./components/App.js";
 
 // Redux store
 import { Provider } from 'react-redux'
-import { configureStore } from './src/store'
+import { configureStore } from './store'
 
 const store = configureStore()
 
