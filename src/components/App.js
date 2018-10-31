249ea007e7588d9958f6c803e304eba1cb14b0ff
d-beloved <morayodeji@gmail.com>
2018-10-31 13:26:28 +0100

    fix: state management bug fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Homepage from './Homepage/Homepage';
-import { SignUp } from './Signup/Signup';
-import { Signin } from './Signin/Signin';
+import SignUp from './Signup/Signup';
+import Signin from './Signin/Signin';
 
 /**
  * @class App
--- src/tests/components/Homepage.test.js
+++ src/tests/components/Homepage.test.js
@@ -3,7 +3,7 @@ import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import expect from 'expect';
 
-import Homepage from '../components/Homepage/Homepage';
+import Homepage from '../../components/Homepage/Homepage';
 
 configure({ adapter: new Adapter() });
 
